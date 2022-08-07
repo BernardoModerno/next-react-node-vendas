b7d7f437869e99d19d333e547a64355c1db76210
Bernardo Moderno <bernardo.moderno@gmail.com>
2022-08-07 15:52:09 -0300

    Permanecendo login

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useState, useEffect } from 'react';
 import { api } from '../services/apiClient';
 import { destroyCookie, setCookie, parseCookies } from 'nookies'
 import Router from 'next/router';
@@ -18,6 +18,31 @@ export function signOut(){
 export function AuthProvider({ children }){
   const [user, setUser] = useState()
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+
+    // tentar pegar algo no cookie
+    const { '@nextauth.token': token } = parseCookies();
+
+    if(token){
+      api.get('/me').then(response => {
+        const { id, name, email } = response.data;
+
+        setUser({
+          id,
+          name,
+          email
+        })
+
+      })
+      .catch(() => {
+        //Se deu erro deslogamos o user.
+        signOut();
+      })
+    }
+
+
+  }, [])
 
   async function signIn({ email, password }){
     try{
